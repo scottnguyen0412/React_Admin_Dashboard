edae8cc9b81753f6306c267a11420f0913c63396
scottnguyen0412 <scottnguyen0412@gmail.com>
2023-02-04 15:39:14 +0700

    Create theme with light and dark mode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import {colorModeContext, useMode} from './theme';
+import {CssBaseLine, ThemeProvider} from '@mui/material';
+// import {Navbar} from './scenes/global/Navbar.jsx';
 
 function App() {
+  const[theme, colorMode] = useMode();
   return (
-    <div className="app">
-      
-    </div>
+    // Use a Provider to pass the current theme
+    <colorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        {/* Reset Css */}
+        <CssBaseLine/>
+        <div className="app">
+          <main className='content'>
+            {/* <Navbar/> */}
+          </main>
+        </div>
+      </ThemeProvider>
+    </colorModeContext.Provider>
   );
 }
 
---
+++ src/scenes/global/Navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+    return <div>Topbar</div>
+}
+
+export default Navbar;
